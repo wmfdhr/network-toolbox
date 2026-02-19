fc7ea02d8d5f691a900afd30dd356477b2d78574
wmfdhr <wmfdhr@users.noreply.github.com>
2026-02-19 22:52:21 +0800

    优化启动脚本，使用pythonw命令

--- 启动.bat
+++ 启动.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-"D:\python all\python 3.13.3\pythonw.exe" "updater.py"
-start "" "D:\python all\python 3.13.3\pythonw.exe" "app_main.py"
+pythonw updater.py
+start "" pythonw app_main.py
